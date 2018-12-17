61707f442bab2efc986ee5b42fa10c2b58053cdc
川崎暑士 <kawasaki8910@gmail.com>
2018-12-17 11:02:41 +0900

    迷路描画

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 fig = plt.figure(figsize=(5,5))
 ax  = plt.gca()
 
+############# 迷路描画 #############
 plt.plot([1, 1], [0, 1], color='red', linewidth=2)
+plt.plot([1, 2], [2, 2], color='red', linewidth=2)
+plt.plot([2, 2], [2, 1], color='red', linewidth=2)
+plt.plot([2, 3], [1, 1], color='red', linewidth=2)
+
+# 目盛り設定
+ax.set_xlim(0, 3)
+ax.set_ylim(0, 3)
+
+plt.text(0.5, 2.5, 'S0', size=14, ha='center')
+plt.text(1.5, 2.5, 'S1', size=14, ha='center')
+plt.text(2.5, 2.5, 'S2', size=14, ha='center')
+
+plt.text(0.5, 1.5, 'S3', size=14, ha='center')
+plt.text(1.5, 1.5, 'S4', size=14, ha='center')
+plt.text(2.5, 1.5, 'S5', size=14, ha='center')
+
+plt.text(0.5, 0.5, 'S6', size=14, ha='center')
+plt.text(1.5, 0.5, 'S7', size=14, ha='center')
+plt.text(2.5, 0.5, 'S8', size=14, ha='center')
+
+plt.text(0.5, 2.3, 'start', size=14, ha='center')
+
+
+# 現在地の描画
+ax.plot([0.5, 0.5], [2.5, 2.5], marker="o", color="g", markersize="60")
+############# 迷路描画 #############
